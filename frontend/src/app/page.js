b26cf78f53564a6214feab6d3a5fab95c86b3d77
adanwilliamdev <adan.william.dev@gmail.com>
2026-06-01 22:26:07 -0300

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,7 @@
+'use client';
+
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
-
-'use client';
 
 import React, { useEffect, useState, useRef, Fragment, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
