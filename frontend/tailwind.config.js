38e8623c678266c5d57de9d6d3f763c1f14a1a1f
adanwilliamdev <adan.william.dev@gmail.com>
2026-06-01 22:07:40 -0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-const path = require('path');
-
 module.exports = {
   content: [
-    path.join(__dirname, './src/pages/**/*.{js,ts,jsx,tsx,mdx}'),
-    path.join(__dirname, './src/components/**/*.{js,ts,jsx,tsx,mdx}'),
-    path.join(__dirname, './src/app/**/*.{js,ts,jsx,tsx,mdx}'),
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: 'class', // Ativa o suporte correto ao modo escuro via classe manual
   theme: {
     extend: {},
   },
