9f41e923bf8c31301779b13f856d1711869ae973
adanwilliamdev <adan.william.dev@gmail.com>
2026-06-01 22:15:41 -0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,12 +3,7 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   output: 'standalone',
-  // Desabilitar geração estática para todas as rotas
   staticPageGenerationTimeout: 120,
-  // Configuração para rotas dinâmicas
-  experimental: {
-    appDir: true,
-  },
 }
 
 module.exports = nextConfig
